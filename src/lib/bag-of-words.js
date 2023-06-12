710bf717edbfda8345795d915ffed15d80dd4676
PranjalAgni <pranjal.agnihotri6@gmail.com>
2023-06-12 23:58:40 +0530

    :zap: bag-of-words technique

--- src/lib/bag-of-words.js
+++ src/lib/bag-of-words.js
@@ -12,29 +12,19 @@ const getFrequencyMapOfText = (text) => {
   return freqMap;
 };
 
-/** 
-Builds a vocabulary object which contains all the unique
-words across document
-@param {object} vocabulary
-*/
-const buildVocabulary = (vocabulary = {}) => {
-  return (freqMap) => {
-    for (const word in freqMap) {
-      vocabulary[word] = true;
-    }
-  };
-};
-
-/** 
+/**
 @param {object} vocabulary - All the unique words
-@param {object} freqMap - Frequency of each word in a document
+@param {object} text - Text for which we want to build the vector
+@returns {number[]}
 */
-const wordToVector = (vocabulary, freqMap) => {
+const bagOfWordVectorization = (vocabulary, text) => {
+  const freqMap = getFrequencyMapOfText(text);
   const wordToVector = [];
   for (const term in vocabulary) {
     wordToVector.push(freqMap[term] || 0);
   }
 
+  print(wordToVector);
   return wordToVector;
 };
 
@@ -45,25 +35,6 @@ const print = (vector) => {
   console.log(vector);
 };
 
-/**
-1. Calculate the frequencey of both sentence
-2. Build a vocabulary list which contains all the unique words across both sentences
-3. Convert the frequency map to vectors for both sentence
-4. Compute the dot product of the vectors
-5. Compute their magnitude
-6. Finally calculate the cosine similarity value
-@param {string} sentenceA
-@param {string} sentenceB
-*/
-const bagOfWordVectors = (sentenceA, sentenceB) => {
-  const freqMapA = getFrequencyMapOfText(sentenceA);
-  const freqMapB = getFrequencyMapOfText(sentenceB);
-  const vocabulary = {};
-  const addToVocabulary = buildVocabulary(vocabulary);
-  addToVocabulary(freqMapA);
-  addToVocabulary(freqMapB);
-  const vectorA = wordToVector(vocabulary, freqMapA);
-  const vectorB = wordToVector(vocabulary, freqMapB);
+module.exports = {
+  bagOfWordVectorization
 };
-
-module.exports = bagOfWordVectors;
