33179df192d20613bbf7287acda1037ac88de109
PranjalAgni <pranjal.agnihotri6@gmail.com>
2023-07-14 23:46:41 +0530

    :hammer: fixed the error

--- src/services/similarity.service.js
+++ src/services/similarity.service.js
@@ -1,21 +1,27 @@
 const { fetchCommentsOfVideo } = require("../libs/platform/youtube");
 const hf = require("../libs/embeddings/hf");
 const { compareEmbeddings } = require("../utils/compare");
+const APIError = require("../exceptions/ApiError");
+const { HttpStatusCode } = require("../constants");
 
 const findSimilarYoutubeComments = async (videoId) => {
-  const commentsList = await fetchCommentsOfVideo(videoId);
-  const commentsWithEmbeddings = await Promise.all(
-    commentsList.map(async (comment) => {
-      const embeddings = await hf.fetchEmbeddings(comment);
-      return {
-        text: comment,
-        embeddings
-      };
-    })
-  );
+  try {
+    const commentsList = await fetchCommentsOfVideo(videoId);
+    const commentsWithEmbeddings = await Promise.all(
+      commentsList.map(async (comment) => {
+        const embeddings = await hf.fetchEmbeddings(comment);
+        return {
+          text: comment,
+          embeddings
+        };
+      })
+    );
 
-  const similarYoutubeComments = compareEmbeddings(commentsWithEmbeddings);
-  return similarYoutubeComments;
+    const similarYoutubeComments = compareEmbeddings(commentsWithEmbeddings);
+    return similarYoutubeComments;
+  } catch (err) {
+    throw new APIError(err.message, HttpStatusCode.INTERNAL_SERVER, err, true);
+  }
 };
 
 module.exports = {
